93d29915804704d63be3eeddc0cb1de728c4163a
leo <82544800+achedon12@users.noreply.github.com>
2024-08-18 15:20:41 +0200

    feat/introduction (#1)
    * [feat] : introduction page
    * [feat] : introduction page with modal
    * [feat] : start adding shop

---
+++ src/components/HotBar/Settings/Settings.js
@@ -0,0 +1,37 @@
+import {CogOutline, PricetagOutline} from "react-ionicons";
+import {useEffect, useState} from "react";
+import '../../../assets/css/settings.css';
+import Modal from 'react-modal';
+
+const Settings = ({showSettings, setShowSettings}) => {
+
+    const [showModal, setShowModal] = useState(showSettings);
+
+    useEffect(() => {
+        setShowSettings();
+    }, [showModal]);
+
+    useEffect(() => {
+        setShowModal(showSettings);
+    }, [showSettings]);
+
+    return (
+        <div className="settings">
+            <CogOutline
+                color={'#000000'}
+                height="30px"
+                width="30px"
+                onClick={() => setShowModal(!showModal)}
+            />
+            <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)}>
+                <div className="settings-content">
+                    <h1>Settings</h1>
+                    <p>Settings content here</p>
+                </div>
+            </Modal>
+        </div>
+    );
+
+};
+
+export default Settings;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,39 @@
-import Heroe from "../components/Characters/Heroe";
+import React, {useState, useEffect} from 'react';
+import Introduction from "./Introduction";
+import Environment from "./Environment";
+import music from "../assets/musics/default.mp3";
+import HotBar from "../components/HotBar/HotBar";
 
 const App = () => {
+    const [intro, setIntro] = useState(true);
+    const [playing, setPlaying] = useState(false);
+    const [audio, setAudio] = useState(null);
+
+    useEffect(() => {
+        const audio = new Audio(music);
+        audio.loop = true;
+        setAudio(audio);
+    }, []);
+
+    useEffect(() => {
+        if (!audio) return;
+        if (playing) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }, [playing]);
+
     return (
         <>
-            <Heroe name="Superman"/>
+            {!intro && (<HotBar playing={playing} setPlaying={setPlaying} audio={audio}/>)}
+            {intro ?
+                <Introduction
+                    setIntro={() => setIntro(false)}
+                    onPlayClick={() => setPlaying(true)}
+                />
+                : <Environment/>
+            }
         </>
     );
 }
